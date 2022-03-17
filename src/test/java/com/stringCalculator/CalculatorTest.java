c6883009e2c46f300d3c36b2c71225d3982d6985
Saurav Gore <sauravgore97@gmail.com>
2022-03-17 13:35:01 +0530

    Create ninth test

--- src/test/java/com/stringCalculator/CalculatorTest.java
+++ src/test/java/com/stringCalculator/CalculatorTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import stringCalculatorTDD.Calculator;
 
 public class CalculatorTest {
+    // Test - 1	
     // Task - 1: Create a simple String calculator with a method signature:
     // ———————————————
     // int Add(string numbers)
@@ -16,65 +17,74 @@ public class CalculatorTest {
     public void shouldReturnZeroOnEmptyString() {
         assertEquals(0, Calculator.add(""));
     }
-
-    // TODO: for single string it will return same string
-	@Test
-	public void shouldReturnNumberOnSingleString() {
-		assertEquals(1, Calculator.add("1"));
-	}
-
-    // TODO: for two strings separated by commas it will return sum of the two
-	@Test
-	public void shouldReturnSumOfNumbersOnTwoStrings() {
-		assertEquals(3, Calculator.add("1,2"));
-	}
     
+    // Test - 2
+    // TODO: for single string it will return same string
+    @Test
+    public void shouldReturnNumberOnSingleString() {
+	assertEquals(1, Calculator.add("1"));
+    }
+
+    // Test - 3
+    // TODO: for two strings separated by commas it will return sum of the two
+    @Test
+    public void shouldReturnSumOfNumbersOnTwoStrings() {
+	assertEquals(3, Calculator.add("1,2"));
+    }
+    
+    //  Test - 4
     //	Task - 2: Allow the Add method to handle an unknown amount of numbers
-	//	TODO: for multiple strings separated by commas it will return sum of all
-	@Test
-	public void shouldReturnSumOfAllNumbers() {
-		assertEquals(6, Calculator.add("1,2,3"));
-	}
+    //	TODO: for multiple strings separated by commas it will return sum of all
+    @Test
+    public void shouldReturnSumOfAllNumbers() {
+	assertEquals(6, Calculator.add("1,2,3"));
+    }
 
+    //  Test - 5
     //	Task - 3: Allow the Add method to handle new lines between numbers (instead of commas)
-	//	TODO: Allow \n as delimiter along-with comma
-	@Test
-	public void shouldAllowNewLineAsDelimiter() {
-		assertEquals(6, Calculator.add("1\n2,3"));
-	}
+    //	TODO: Allow \n as delimiter along-with comma
+    @Test
+    public void shouldAllowNewLineAsDelimiter() {
+	assertEquals(6, Calculator.add("1\n2,3"));
+    }
 
+    //  Test - 6
     //	Task - 4: Support different delimiters
-	//	TODO: Allow custom delimiter by checking // in beginning of first line
-	@Test
-	public void shouldAllowCustomDelimiter() {
-		assertEquals(3, Calculator.add("//;\n1;2"));
-	}
+    //	TODO: Allow custom delimiter by checking // in beginning of first line
+    @Test
+    public void shouldAllowCustomDelimiter() {
+	assertEquals(3, Calculator.add("//;\n1;2"));
+    }
 
+    //  Test - 7
     //	TODO: Custom Delimiter can be custom Regex Character
-	@Test
-	public void shouldAllowRegexCharAsCustomDelimiter() {
-		assertEquals(3, Calculator.add("//.\n1.2"));
-	}
+    @Test
+    public void shouldAllowRegexCharAsCustomDelimiter() {
+	assertEquals(3, Calculator.add("//.\n1.2"));
+    }
 
+    //  Test - 8
     //	Task - 5: Throw Exception for negative numbers
-	//	TODO: Throw exception for negative numbers
-	@Test
-	public void shouldThrowExceptionForNegativeNumbers() {
-		try {
-			Calculator.add("1,-2,3");
-			fail("Exception expected");
-		}
+    //	TODO: Throw exception for negative numbers
+    @Test
+    public void shouldThrowExceptionForNegativeNumbers() {
+	try {
+		Calculator.add("1,-2,3");
+		fail("Exception expected");
+	}
         catch(RuntimeException e) {}
-	}
+    }
+	
+    //  Test - 9
     //	TODO: Exception message should have negative number
-	@Test
-	public void shouldHaveNegativeNumbersInException() {
-		try {
-			Calculator.add("-1,-2,3");
-			fail("Exception expected");
-		}
-        catch(RuntimeException e) {
-			assertEquals("negatives not allowed: -1, -2", e.getMessage());
-		}
+    @Test
+    public void shouldHaveNegativeNumbersInException() {
+	try {
+		Calculator.add("-1,-2,3");
+		fail("Exception expected");
 	}
+        catch(RuntimeException e) {
+		assertEquals("negatives not allowed: -1, -2", e.getMessage());
+	}
+    }
 }
